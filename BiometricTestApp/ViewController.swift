e6df1c5a58bf0841f4ef18d81af72d3978ad1545
문종식 <moonjs@MacBookPro.local>
2021-10-25 09:51:18 +0900

    Create Project

---
+++ BiometricTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BiometricTestApp
+//
+//  Created by 문종식 on 2021/10/25.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
